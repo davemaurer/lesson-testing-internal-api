213370eede48ce88e5826cdfd0c512cd61baa364
David Maurer <davemaurermail@gmail.com>
2015-09-15 09:21:52 -0600

    add parsed response body to items controller test

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -4,6 +4,8 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
   test '#index' do
     get :index, format: :json
 
+    items = JSON.parse(response.body)
+
     assert_response :success
     # or assert_equal 200, response.status
   end
