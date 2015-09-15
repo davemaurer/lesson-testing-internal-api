02bd81b46c177467260de4e7cf26e1e3d0bf4755
David Maurer <davemaurermail@gmail.com>
2015-09-15 09:53:44 -0600

    added create and show with tests

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::ItemsController < ApplicationController
   def index
     respond_with Item.all
   end
+
+  def show
+    respond_with Item.find_by(params[:id])
+  end
+
+  def create
+    # use location: nil to ignore url location
+    respond_with  Item.create(item_params), location: nil
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
 end
--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -4,12 +4,32 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
   test '#index' do
     get :index, format: :json
 
-    items = JSON.parse(response.body)
+    #symbolize_names: true lets you use :name instead of 'name' in tests
+    items = JSON.parse(response.body, symbolize_names: true)
     item = items.first
 
     assert_response :success
     # or assert_equal 200, response.status
-    assert_equal 2, items.count
-    assert_equal 'Lannister', item['name']
+    assert_equal 2,                    items.count
+    assert_equal 'Lannister',          item[:name]
+    assert_equal 'My uncle is my dad', item[:description]
+  end
+
+  test '#show' do
+    #need to identify this get with an id, as below
+    get :show, format: :json, id: Item.first.id
+
+    item = JSON.parse(response.body, symbolize_names: true)
+    assert_response :success
+    assert_equal 'Lannister',          item[:name]
+    assert_equal 'My uncle is my dad', item[:description]
+  end
+
+  test '#create' do
+    #create the params you will get back, then associate them with item: in the post method
+    item_params = { name: 'Arya', description: 'Is not blind' }
+    post :create, format: :json, item: item_params
+
+    assert_response :success
   end
 end
