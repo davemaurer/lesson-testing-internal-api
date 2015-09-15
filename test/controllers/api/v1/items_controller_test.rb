4028e9e36e33d7d125679a68097328dc3b3e8567
David Maurer <davemaurermail@gmail.com>
2015-09-15 09:25:22 -0600

    altered fixtures and added assertions to items controller test for names and count

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -5,8 +5,11 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
     get :index, format: :json
 
     items = JSON.parse(response.body)
+    item = items.first
 
     assert_response :success
     # or assert_equal 200, response.status
+    assert_equal 2, items.count
+    assert_equal 'Lannister', item['name']
   end
 end
